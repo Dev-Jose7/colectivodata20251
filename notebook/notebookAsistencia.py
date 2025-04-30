00c70312102708b61b17bd3a26621c9cd6302d11
Jose Navarro <jfnr398@gmail.com>
2025-04-29 19:31:25 -0500

    chore: se añaden filtros para examen

--- notebook/notebookAsistencia.py
+++ notebook/notebookAsistencia.py
@@ -11,4 +11,29 @@ asistenciaDataFrame = pd.read_csv("./data/asistencia_estudiantes_completo.csv")
 # print(asistenciaDataFrame.describe())
 # print(asistenciaDataFrame.isnull().sum())
 # print(asistenciaDataFrame["estado"].value_counts())
-print(asistenciaDataFrame["estrato"].value_counts().head(2))
+# print(asistenciaDataFrame["estrato"].value_counts().head(2))
+
+## FILTRO O CONSULTAS DETALLADAS
+# Necesito encontrar los estudiantes que asistieron
+# Necesito encontrar los estudiantes que faltaron
+# Necesito encontrar los estudiantes que llegaron tarde, es decir que justificarón la inasistencia
+# Necesito encontrar los estudiantes de estrato 1
+# Necesito encontrar los estudiantes de estratos altos
+# Necesito encontrar estudiantes que llegan en metro
+# Necesito encontrar estudiantes que llegarón en bicicleta
+# Necesito encontrrar todos los estudiantes menos los que llegarón a pie
+# Necesito todos los registros de asistencia de Junio
+# Necesito todos los estudiantes que usan transportes ecologicos
+# Necesito los que usan bus y son de estrato alto
+# Necesito los que usan bus y son de estrato bajo
+# Necesito estudiantes que caminan para llegar a clase
+
+## CONTEOS POR AGRUPACIONES
+# Necesito el conteo de registros por estado de asistencia 
+# Necesito obtener el número de registro por estrato
+# Necesito la agrupar cantidad de estudiante por medio de transporte
+# Necesito el promedio del estrato por estado de asistencia
+# Máximo estrato por estado
+# Minimo estrato por estado
+# Necesito un conteo de asistencia por grupo y estado
+
--- notebook/notebookUsuarios.py
+++ notebook/notebookUsuarios.py
@@ -4,3 +4,14 @@ usuariosDataFrame = pd.read_excel("./data/usuarios_sistema_completo.xlsx")
 
 # print(usuariosDataFrame)
 print(usuariosDataFrame.isnull().sum())
+
+## FILTRO
+# Necesito solo un listado de aprendices.
+# Necesito un listado con instructores
+# Necesito un listado de especialista en desarrollo web o sistema
+# Necesito un listado de usuarios con direcciones solo en Medellín
+# Necesito un listado de solo usuarios cuya direcciones terminen en sur
+# Necesito un listado de especialista que contengan la palabra "Datos" en la especialidad
+# Necesito una lista de nacidos antes de 1990
+# Necesito un listado de instructores son ancianos
+# Neceisto un listado de instructores nacidos en el nuevo milenio
